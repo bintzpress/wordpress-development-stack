38f446dc4120648d209ad0f07fbf79f5378a8c30
Brian Bintz <brian@bintzpress.com>
2022-01-13 23:02:05 +0000

    fixes to get wp-cli working... never figured it out. falling back to git instead of https for some reason.

--- wordpress-projects/bin/developer_setup.sh
+++ wordpress-projects/bin/developer_setup.sh
@@ -7,24 +7,14 @@ on_error() {
     exit 1
 }
 
-# Setup composer and wp-cli
-echo 'PATH=$PATH:/home/developer/.config/composer/vendor/bin' >> /home/developer/.bashrc
+# Setup wp-cli
 cd /home/developer
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === '906a84df04cea2aa72f40b5f787e49f22d4c2f19492ac310e8cba5b96ac8b64115ac402c8cd292b8a03482574915d1a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-mkdir -p /home/developer/.config/composer/vendor/bin
-mv composer.phar /home/developer/.config/composer/vendor/bin/composer
-export PATH=$PATH:/home/developer/.config/composer/vendor/bin
-composer config --global github-protocols https
-composer global require wp-cli/wp-cli-bundle
 mkdir /home/developer/.wp-cli
 echo "path: /var/www/html" >> /home/developer/.wp-cli/config.yml
 wp package install wp-cli/restful
-wp package install bintzpress/wp-cli-fse-support:@stable
+wp package install https://github.com/bintzpress/wp-cli-fse-support.git
 
-# Setup Grunt and it's dependencies
+# Setup stuff for building
 curl -L https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh -o install.sh
 chown developer:developer install.sh
 chmod ug+x install.sh
